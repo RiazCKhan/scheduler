6c7917458fef7d9654b1608501cf26da2f4f881b
RiazK <RiazCKhan@gmail.com>
2021-11-12 09:13:39 -0500

    Add anon onClick setDay

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DayListItem(props) {
   return (
-    <li>
+    <li onClick={() => props.setDay(day)}>
       <h2 className="text--regular">Day Name</h2> 
       <h3 className="text--light">X spots remaining</h3>
     </li>
