54317f961ab48941aed0f55127203c95ba6bc7a7
RiazK <RiazCKhan@gmail.com>
2021-11-12 10:19:57 -0500

    Add conditional style to daylistitem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,10 +4,16 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2>Day Name</h2> 
+      <h3>X spots remaining</h3>
     </li>
   );
 }
