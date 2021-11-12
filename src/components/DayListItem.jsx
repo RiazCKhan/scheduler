77b27a4e3fb500b3354be2afca22d913b6655a34
RiazK <RiazCKhan@gmail.com>
2021-11-12 09:16:38 -0500

    Update setDay param

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DayListItem(props) {
   return (
-    <li onClick={() => props.setDay(day)}>
+    <li onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">Day Name</h2> 
       <h3 className="text--light">X spots remaining</h3>
     </li>
