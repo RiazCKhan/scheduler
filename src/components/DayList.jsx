4237f8c31adcab0419c36693b01f7157f4f0e841
RiazK <RiazCKhan@gmail.com>
2021-11-12 13:37:27 -0500

    Quick Save - Code Review

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+
+  const daysArray = props.days
+
+  // console.log(daysArray)
+  // console.log(props.days)
+
+  const days = daysArray.map((day) => {
+    // console.log(daysArray)
+    // console.log(days)
+    // console.log(day)
+
+    return (
+      <DayListItem
+        key={day.id}
+        day={day.name}
+        spots={day.spots}
+      />
+    )
+  })
+
+
   return (
-    <>
-    </>
-  )
+    <ul>
+      {days}
+    </ul>
+  );
 }
