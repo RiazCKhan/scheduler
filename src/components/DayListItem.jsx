7bdbcc79943d0185965895096589e9ff583a7500
RiazK <RiazCKhan@gmail.com>
2021-11-12 12:28:10 -0500

    Implement conditional spots remaining logic

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -5,6 +5,20 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 
+  const { spots } = props
+
+  const formatSpots = (spots) => {
+    if (spots > 1) {
+      return `${spots} spots remaining`
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    }
+    return "no spots remaining"
+  }
+
+  const spotsRemaining = formatSpots(spots)
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
@@ -12,8 +26,8 @@ export default function DayListItem(props) {
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
-      <h2>Day Name</h2> 
-      <h3>X spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsRemaining}</h3>
     </li>
   );
 }
