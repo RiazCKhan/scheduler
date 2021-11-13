dd7e09ffecdfbddd1a873ac8624a71fc88c2355c
RiazK <RiazCKhan@gmail.com>
2021-11-13 12:58:28 -0500

    Alter dynamic render of InterviewListItem

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,11 +1,32 @@
-import react from "react";
+import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss"
 
 export default function interviewerList(props) {
+
+  // console.log(props) 
+
+  const interviewersArray = props.interviewers
+
+  const interviewers = interviewersArray.map((interviwer) => {
+    return (
+      <InterviewerListItem
+        key={interviwer.id}
+        name={interviwer.name}
+        avatar={interviwer.avatar}
+        id={interviwer.id}
+        selected={interviwer.id === props.interviewer}
+        setInterviewer={props.setCurrentInterviewerID}
+      />
+    );
+  })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
     </section>
   )
 }
