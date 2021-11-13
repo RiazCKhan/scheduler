244c6afd901aa27483d0c890d29d511313dd297f
RiazK <RiazCKhan@gmail.com>
2021-11-13 11:03:45 -0500

    Implement Conditional CSS and name render

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 
+import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
+
 export default function InterviewerListItem(props) {
+
+  console.log(props)
+
+
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+
+    /* 
+    interviewers__item-image
+    interviewers__item--selected
+    interviewers__item--selected-image
+    */
+  })
+
   return (
-    <li className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   )
 }
+
+
+// const interviewer = {
+//   id: 1,
+//   name: "Sylvia Palmer",
+//   avatar: "https://i.imgur.com/LpaY82x.png"
+// };
