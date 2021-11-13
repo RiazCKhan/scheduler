78361c3f7bbb3235224c1df5b1ac3fc96e516d9d
RiazK <RiazCKhan@gmail.com>
2021-11-13 17:25:28 -0500

    Add Appointment Story Module

--- stories/index.js
+++ stories/index.js
@@ -6,10 +6,14 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+
+import Appointment from "components/Appointment/index.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -133,3 +137,11 @@ storiesOf("InterviewerList", module)
       onChange={action("setCurrentInterviewerID")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
+
