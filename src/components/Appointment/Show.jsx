b209a76b08496af7f217fad63b073347e57ee9a1
RiazK <RiazCKhan@gmail.com>
2021-11-15 17:30:56 -0500

    Render via Props

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function Show(props) {
+  // console.log(props.interviwers)
+  const interviewersArray = props.interviewers
+  // console.log(interviewersArray[0].name)
+
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">Lydia Miller-Jones</h2>
+        <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">{interviewersArray[0].name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
@@ -16,11 +20,13 @@ export default function Show(props) {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
+            onClick={props.onEdit}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
+            onClick={props.onDelete}
           />
         </section>
       </section>
