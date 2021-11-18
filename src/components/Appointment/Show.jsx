3cd250db792b7007c2bd1bcf0be0177e3ff318ac
RiazK <RiazCKhan@gmail.com>
2021-11-17 20:30:12 -0500

    Update the interviewer name prop

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Show(props) {
   // console.log(props.interviwers)
-  const interviewersArray = props.interviewers
+  // const interviewersArray = props.interviewers
   // console.log(interviewersArray[0].name)
-
+  // console.log('show component:', props)
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
         <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewersArray[0].name}</h3>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
