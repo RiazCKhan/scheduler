e4be22b9f30bfa127c0013dc29c8e3e900061141
RiazK <RiazCKhan@gmail.com>
2021-11-21 18:12:01 -0500

    Implement transition FN, passing all tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,11 @@
-export default function useVisualMode() { 
+import React, { useState } from "react";
 
-}
+export default function useVisualMode(inital) {
+  const [mode, setMode] = useState(inital)
+
+  function transition(value) {
+    setMode(value)
+  }
+
+  return { mode, transition }
+};
