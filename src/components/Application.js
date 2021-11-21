3303e54353666b7d5f32c02671959f1190957af2
RiazK <RiazCKhan@gmail.com>
2021-11-21 11:22:45 -0500

    Add Promise All to Axios API call

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,45 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -54,16 +16,24 @@ export default function Application(props) {
 const dailyAppointments = [];
 
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState(prev => ({ ...prev, days}));
+  // const setDays = (days) => setState(prev => ({ ...prev, days}));
 
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      // console.log(response.data)
-      setDays(([...response.data]))
-    });
+    const apiDays = axios.get("/api/days")
+    const apiAppointments = axios.get("/api/appointments")
+    // const apiInterviewers --- final end point = "/api/interviewers"
+
+    Promise.all([
+      apiDays,
+      apiAppointments
+    ]).then((all) => {
+      console.log('im on everything', all)
+      setState(prev => ({ ...prev, apiDays: all[0].data, apiAppointments: all[1].data }))
+    }).catch((error) => {
+      console.log("promise all api resolution error:", error)
+    })
   }, []);
 
-  // const appointmentsArr = Object.values(appointments)
   const appointment = dailyAppointments.map((appt) => {
     // console.log(appt.interview)
     return (
