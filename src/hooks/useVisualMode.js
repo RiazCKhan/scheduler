6243966dcc77e1492122ebb867cc28241146f8cd
RiazK <RiazCKhan@gmail.com>
2021-11-22 12:18:15 -0500

    Mentor Splice() Solution + Student Pop() Solution

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,28 @@ import React, { useState } from "react";
 
 export default function useVisualMode(inital) {
   const [mode, setMode] = useState(inital)
+  const [history, setHistory] = useState([inital]);
 
   function transition(value) {
+    setHistory([inital, mode, value])
     setMode(value)
+
+    // console.log('init 1 then 2 transition', mode)
+    // console.log('Transition FN History ARR', history)
   }
 
-  return { mode, transition }
+  function back() {
+    history.pop()
+    // console.log('Back FN History ARR', history)
+    setMode(history[history.length - 1])
+
+    // Mentor: Lovemore Solution - BELOW
+    const newHistory = history.slice(0, history.length - 1)
+    setHistory(newHistory)
+    setMode(newHistory[newHistory.length - 1])
+  }
+
+// EDGE CASE - What happens IF History ARR only has one item? Back() would run into an empty History [];
+
+  return { mode, transition, back }
 };
