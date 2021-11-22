0d5d18b22485c991795d36487d04c2b351bb700b
RiazK <RiazCKhan@gmail.com>
2021-11-22 16:12:14 -0500

    Implement and Test getInterviewersForDay. All tests passing

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -40,6 +40,48 @@ export function getAppointmentsForDay(state, day) {
   return appointmentArrayObj
 }
 
+export function getInterviewersForDay(state, day) {
+
+  // Validation: if days data is empty
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  const filteredByDay = state.days.filter(element => {
+    return element.name === day
+  });
+
+  // console.log(filteredByDay)
+
+  // Validation: if no day found 
+  if (filteredByDay.length === 0) {
+    return [];
+  };
+
+  const filteredDayAppointmentArray = filteredByDay[0].appointments
+
+  // Function will convert filteredDayAppointmentArray of numbers to array of strings
+  const filteredAppointmentArray = filteredDayAppointmentArray.map(element => {
+    return String(element)
+  });
+
+  // Function will match appointments based on day 
+  const matchedAppointmentID = [];
+  const filteredAppointmentID = filteredAppointmentArray.filter(element => {
+    if (filteredAppointmentArray.includes(element)) {
+      matchedAppointmentID.push(element)
+    }
+  });
+
+  // Function will return an array of appointment details/obj
+  const appointmentArrayObj = [];
+  matchedAppointmentID.forEach(element => {
+    appointmentArrayObj.push(state.appointments[element])
+  });
+
+  return appointmentArrayObj
+}
+
 export function getInterview(state, interview) {
   // console.log('ze state', state.appointments)
   // console.log('ze interview', interview)
