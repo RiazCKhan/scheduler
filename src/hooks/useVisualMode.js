fd7a378727adfa9d9c13866992ae7cbb30422d24
RiazK <RiazCKhan@gmail.com>
2021-11-22 14:05:35 -0500

    Add Transition with Replace Test + Implement code. All tests passing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,19 +4,22 @@ export default function useVisualMode(inital) {
   const [mode, setMode] = useState(inital)
   const [history, setHistory] = useState([inital]);
 
-  function transition(value) {
-    setHistory([inital, mode, value])
-    setMode(value)
-  }
+  function transition(value, replace = false) {
+    setHistory([inital, mode, value]);
+    if (replace === true) {
+      setHistory([inital, mode]);
+    }
+    setMode(value);
+  };
 
   function back() {
-    history.pop()
+    history.pop();
     // EDGE CASE - What happens IF History ARR only has one item? Back() would run into an empty History [];
     if (history.length === 0) {
       return undefined;
     }
-    setMode(history[history.length - 1])
-  }
+    setMode(history[history.length - 1]);
+  };
 
   return { mode, transition, back }
 };
