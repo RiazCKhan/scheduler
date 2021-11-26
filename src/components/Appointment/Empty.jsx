9dcd809c8e117c7c4965e29adc85708123be9b0e
RiazK <RiazCKhan@gmail.com>
2021-11-26 17:40:47 -0500

    Moved prop decleration to function level - bugfix

--- src/components/Appointment/Empty.jsx
+++ src/components/Appointment/Empty.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const { onAdd } = props
 
 export default function Empty(props) {
+  const { onAdd } = props
   return (
     <main className="appointment__add">
       <img
