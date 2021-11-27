ea81b42d30ea7aefadb0bbd1141cbb2d06b5db2b
RiazK <RiazCKhan@gmail.com>
2021-11-27 17:21:27 -0500

    seperation of concerns, moved logic to custom hook 'useAppData'

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,83 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
+import useApplicationData from "hooks/useApplicationData";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-       // console.log('axios Data Check', all[1].data)
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
-    }).catch((error) => {
-      console.log("CATCH all api resolution error:", error)
-    })
-  }, []);
-
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, { interview })
-      .then((result) => {
-        setState({
-          ...state,
-          appointments
-        })
-      }).catch((error) => {
-        console.log("CATCH axios put error", error)
-        return Promise.reject(error);
-      })
-  }
-
-  const cancelInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-  
-    return axios.delete(`/api/appointments/${id}`)
-      .then((result) => {
-        setState({
-          ...state,
-          appointments
-        })
-      }).catch((error) => {
-        console.log("CATCH axios delete error", error)
-        return Promise.reject(error);
-      })
-  }
-
-  const setDay = (day) => setState({ ...state, day });
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData(); 
 
   const dailyAppointments = getAppointmentsForDay(state, state.day)
+  const interviewers = getInterviewersForDay(state, state.day)
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview)
-    const interviewers = getInterviewersForDay(state, state.day)
 
     return (
       <Appointment
