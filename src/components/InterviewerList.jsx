b8c2031304608a8f4d47057a80d7aa15004e749e
RiazK <RiazCKhan@gmail.com>
2021-12-02 11:11:41 -0500

    Import PropTypes + TypeCheck InterviewList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function interviewerList(props) {
   const interviewersArray = props.interviewers
@@ -28,3 +29,7 @@ export default function interviewerList(props) {
     </section>
   )
 }
+
+interviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
