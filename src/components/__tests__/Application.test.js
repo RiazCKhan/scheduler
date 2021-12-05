44f095c84c8828c79d99f94331a8b167e56c33d1
RiazK <RiazCKhan@gmail.com>
2021-12-05 15:40:13 -0500

    Checkpoint: save and PUT request functioning

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,21 +1,88 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { 
+  render, 
+  cleanup, 
+  waitForElement,
+  fireEvent,
+  getByText, 
+  getAllByTestId, 
+  getByAltText, 
+  getByPlaceholderText, 
+  prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Application", () => {
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+  xit("renders without crashing", () => {
+    render(<Application />);
+  });
 
-  return waitForElement(() => getByText("Monday"))
-  .then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday"))
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      })
+  });
+
+
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+
+    // 1. Render App
+    // 2. Wait until Archie COhen is displayed, i.e., app is loaded
+    // 3. Click "Add Button" on the first empty appointment
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    // 5. Click the first interviewer in the list.
+    // 6. Click the "Save" button on that same appointment.
+    // 7. Check that the element with the text "Saving" is displayed.
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    // console.log(prettyDOM(container));
+
+    const appointments = getAllByTestId(container, "appointment")
+    // console.log(prettyDOM(appointments));
+
+    const appointment = appointments[0]
+    // console.log(prettyDOM(appointment));
+
+    fireEvent.click(getByAltText(appointment, "Add"))
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    })
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+
+
   })
-});
+})
+
+
+// const emptyAppointmentSlots = queryAllByTestId("empty-appointment-slot-button")
+// if (emptyAppointmentSlots.length) {
+//   let firstAppointmentButton = emptyAppointmentSlots[0]
+//   fireEvent.click(firstAppointmentButton)
+// }
+
+// const input = getByPlaceholderText("Enter Student Name");
+// fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } })
+
+// const interviewerList = queryAllByTestId("interviewer-list")
+// if (interviewerList.length) {
+//   let firstInterviewer = interviewerList[0]
+//   fireEvent.click(firstInterviewer)
+// }
